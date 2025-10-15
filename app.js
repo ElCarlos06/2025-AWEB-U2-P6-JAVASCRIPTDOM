cde65ca8a7b04c07a61c4bd6b56bc6d0d354b8e5
ElCarlos06 <20243ds085@utez.edu.mx>
2025-10-15 14:58:05 -0600

    ACtividad terminada (segun)

---
+++ app.js
@@ -0,0 +1,22 @@
+const btnCalcular = document.getElementById('btnCalcular');
+const inputLado = document.getElementById('inputLado');
+const resultado = document.getElementById('resultado')
+
+
+btnCalcular.addEventListener('click',
+    function(){
+        const lado = parseFloat(inputLado.value);
+        let area = 0.0;
+
+        if(lado >0){
+            area = lado*lado;
+            resultado.className = 'mensaje-correcto';
+            resultado.textContent = `El area es: ${area}`;
+        }else{
+            resultado.className = 'mensaje-error';
+            resultado.textContent = 'Ingresa un valor mayor a 0';
+        }
+
+
+    }
+)
